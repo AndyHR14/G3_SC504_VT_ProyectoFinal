37c4b8a2c68dd36fd1302d72f8cfc1d41a5f6725
Franciscoffv <franffv0809@gmail.com>
2025-08-06 22:45:05 -0600

    Update animales.php

--- php/animales.php
+++ php/animales.php
@@ -1,17 +1,4 @@
 <?php
-$host = 'localhost'; 
-$dbname = 'Proyecto_Final'; 
-$username = 'Proyecto_Final'; 
-$password = '123'; 
-
-try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
-    exit;
-}
-
 include 'db.php';
 include 'includes/header.php';
 
