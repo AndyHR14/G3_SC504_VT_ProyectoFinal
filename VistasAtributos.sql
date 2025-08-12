96a83d6eb6baf6b796c2e02de601e1679bd2c82d
Alvarado-Majo <mariajosealvarado235@gmail.com>
2025-08-11 21:02:05 -0600

    Update VistasAtributos.sql
    Vistas con los atributos de cada tabla

--- VistasAtributos.sql
+++ VistasAtributos.sql
@@ -1,101 +1,126 @@
 --Vistas de los atributos de cada tabla
 
+--Vista Estados
 CREATE OR REPLACE VIEW vista_fide_estados_tb AS
 SELECT *
 FROM FIDE_ESTADOS_TB;
 
+--Vista Genero
 CREATE OR REPLACE VIEW vista_fide_genero_tb AS
 SELECT *
 FROM FIDE_GENERO_TB;
 
+--Vista Tipo
 CREATE OR REPLACE VIEW vista_fide_tipo_tb AS
 SELECT *
 FROM FIDE_TIPO_TB;
 
+--Vista Habitat
 CREATE OR REPLACE VIEW vista_fide_habitat_tb AS
 SELECT *
 FROM FIDE_HABITAT_TB;
 
+--Vista Rutina
 CREATE OR REPLACE VIEW vista_fide_rutina_tb AS
 SELECT *
 FROM FIDE_RUTINA_TB;
 
+--Vista Marca Alimento
 CREATE OR REPLACE VIEW vista_fide_marca_alimento_tb AS
 SELECT *
 FROM FIDE_MARCA_ALIMENTO_TB;
 
+--Vista Alimentacion
 CREATE OR REPLACE VIEW vista_fide_alimentacion_tb AS
 SELECT *
 FROM FIDE_ALIMENTACION_TB;
 
+--Vista Animal
 CREATE OR REPLACE VIEW vista_fide_animal_tb AS
 SELECT *
 FROM FIDE_ANIMAL_TB;
 
+--Vista Rol
 CREATE OR REPLACE VIEW vista_fide_rol_tb AS
 SELECT *
 FROM FIDE_ROL_TB;
 
+--Vista Usuario
 CREATE OR REPLACE VIEW vista_fide_usuario_tb AS
 SELECT *
 FROM FIDE_USUARIO_TB;
 
+--Vista Horario
 CREATE OR REPLACE VIEW vista_fide_horario_tb AS
 SELECT *
 FROM FIDE_HORARIO_TB;
 
+--Vista Pais
 CREATE OR REPLACE VIEW vista_fide_pais_tb AS
 SELECT *
 FROM FIDE_PAIS_TB;
 
+--Vista Provincia
 CREATE OR REPLACE VIEW vista_fide_provincia_tb AS
 SELECT *
 FROM FIDE_PROVINCIA_TB;
 
+--Vista Canton
 CREATE OR REPLACE VIEW vista_fide_canton_tb AS
 SELECT *
 FROM FIDE_CANTON_TB;
 
+--Vista Distrito
 CREATE OR REPLACE VIEW vista_fide_distrito_tb AS
 SELECT *
 FROM FIDE_DISTRITO_TB;
 
+--Vista Direccion
 CREATE OR REPLACE VIEW vista_fide_direccion_tb AS
 SELECT *
 FROM FIDE_DIRECCION_TB;
 
+--Vista Telefono
 CREATE OR REPLACE VIEW vista_fide_telefono_tb AS
 SELECT *
 FROM FIDE_TELEFONO_TB;
 
+--Vista Correo
 CREATE OR REPLACE VIEW vista_fide_correo_tb AS
 SELECT *
 FROM FIDE_CORREO_TB;
 
+--Vista Producto
 CREATE OR REPLACE VIEW vista_fide_producto_tb AS
 SELECT *
 FROM FIDE_PRODUCTO_TB;
 
+--Vista Unidad de Medida
 CREATE OR REPLACE VIEW vista_fide_unidad_medida_tb AS
 SELECT *
 FROM FIDE_UNIDAD_MEDIDA_TB;
 
+--Vista Metodo de pago
 CREATE OR REPLACE VIEW vista_fide_metodo_pago_tb AS
 SELECT *
 FROM FIDE_METODO_PAGO_TB;
 
+--Vista Factura
 CREATE OR REPLACE VIEW vista_fide_factura_tb AS
 SELECT *
 FROM FIDE_FACTURA_TB;
 
+--Vista Detalle Factura
 CREATE OR REPLACE VIEW vista_fide_detalle_factura_tb AS
 SELECT *
 FROM FIDE_DETALLE_FACTURA_TB;
 
+--Vista Detalle alimentacion
 CREATE OR REPLACE VIEW vista_fide_detalle_alimentacion_tb AS
 SELECT *
 FROM FIDE_DETALLE_ALIMENTACION_TB;
 
+--Vista Detalle rutina
 CREATE OR REPLACE VIEW vista_fide_detalle_rutina_tb AS
 SELECT *
 FROM FIDE_DETALLE_RUTINA_TB;
